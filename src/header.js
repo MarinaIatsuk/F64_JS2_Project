bb9b4e278a96c52bc48ee7075827cae5051d8e05
slastinatalia <snatasha1591@gmail.com>
2023-11-20 14:46:45 +0300

    список фильмов добавлен

--- src/header.js
+++ src/header.js
@@ -2,49 +2,45 @@
 document.querySelector('#avatar').onclick = function(){
     //alert('вход в личный кабинет');
 }
+
  //const API_KEY = "6e01b98a-32ba-41c9-b64f-a2a9582aafa5";
  //const url = 
  //"https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=movie&page=1";
  
  
- 
- const list = document.querySelector('#list')
- const filter = document.querySelector('#filter') 
- let FILMS = []
-  filter.addEventListener('input', (event) => {
-    const value  = event.target.value.toLowerCase()
-     const filteredFilms = FILMS.filter ((film) => {
-      return film.name.toLowerCase().includes(value)
-    })
-   render(filteredFilms)
-    })
-  async function start(){
-     try{
-       const responce = await fetch('https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${input.value}&page=1',
-       { 
-          method: "GET",
-          headers:{
-          'content-type':     'application/json',
-          "X-API-KEY":"6e01b98a-32ba-41c9-b64f-a2a9582aafa5"},
-       })
-      //       // const data = await  responce.json() вот здесь я это убрала так как это не нужно?         //       render(data)
-          }
-           catch(err){
-        list.style.color ='red'
-         list.innerHTML = err.message
-     }
- }
-  function render(films = []){
-     if(films.length === 0){
-       list.innerHTML = 'Этого фильма нет!'
-     } else{
-     const html = films.map(toHTML).join('')
-        list.innerHTML = html
-     }
-  }
-   function toHTML(film) {
-    return`
-    <li class="list-group-item">${film.name}</li>
-     `  }
+ const filter = document.querySelector("#filter");
+ const list = document.querySelector("#list");
 
-    start()
+ filter.addEventListener("input", (event) => {
+         var keyword = event.target.value.toLowerCase();
+         fetch(
+                 `https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${encodeURIComponent(
+                         keyword
+                 )}&page=1`,
+                 {
+                         method: "GET",
+                         headers: {
+                                 "X-API-KEY": "6e01b98a-32ba-41c9-b64f-a2a9582aafa5",
+                                 "Content-Type": "application/json",
+                         },
+                 }
+         )
+                 .then((res) => res.json())
+                 .then((data) => {
+                         list.innerHTML = "";
+                         data.films.forEach((film) => {
+                                 const li = document.createElement("li");
+                                 li.textContent = film.nameRu;
+                                 list.appendChild(li);
+                         });
+                 })
+                 .catch((err) => console.log(err));
+ });
+
+
+const btnOpen =  function() {
+   document.querySelector('#btnOpen').style.display="none";
+}
+
+const avatar = document.getElementById("avatarId");
+avatar.style.display = "none";
