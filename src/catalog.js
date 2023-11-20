34e95165e0d03de207b89279f493d5066fe02632
Marina Iatsuk <slade2739@gmail.com>
2023-11-20 22:17:32 +0300

    адаптив, добавление стран, стили

--- src/catalog.js
+++ src/catalog.js
@@ -1,13 +1,28 @@
 import * as db from './db'; // для работы с БД
 
-let currentPage = 1; //Для пагинации
-let prevPageBtn, nextPageBtn; //объявление переменных для кнопок переключения страниц здесь, чтобы они работали в функции
+let currentPage = 1; // Флаг для пагинации
+let loading = false; // Флаг для загрузки данных
 
 // Обработчик события клика на кнопку поиска
 document.querySelector(".btn").addEventListener("click", function (event) {
     event.preventDefault(); // Остановить перезагрузку страницы
-    currentPage = 1;  // для того, чтобы вернуть пользователя на первую страницу, при его новом поиске на любой другой
-    getData(currentPage); // Вызов функцию для получения данных с текущей страницы
+    currentPage = 1; // Сбрасываем текущую страницу при новом поиске
+    const container = document.querySelector(".content");
+    container.innerHTML = ""; // Очищаем содержимое контейнера перед загрузкой новых данных
+    getData(currentPage); // Вызов функции для получения данных с текущей страницы
+
+     window.scrollTo({ // Возвращаем скролл в начало страницы
+        top: 0,
+        behavior: 'smooth' // плавный эффект прокрутки
+    });
+});
+
+// Обработчик события прокрутки страницы
+window.addEventListener('scroll', function () {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !loading) { //проверяем, достиг ли пользователь конца документа
+        loading = true; // Достигнут конец страницы, загружаем следующую порцию данных
+        getData(currentPage + 1);
+    }
 });
 
 // Функция для выполнения запроса к API
@@ -21,73 +36,70 @@ async function getData(page) {
             method: "GET",
             headers: {
                 'content-type': "application/json",
-                "X-API-KEY": "ee5367e9-c264-44b7-93b8-dab17fafadc7",
+                "X-API-KEY": "24b15cb0-02d2-47d4-83c9-c601d49f2256",
             },
         });
         const data = await response.json();
         //console.log(data); //Проверка
-        updateContainer(data); //отрисовка списка
+        updateContainer(data); // отрисовка списка
         currentPage = page;
-        
+        loading = false; // разрешаем загрузку следующей порции данных
+
     } catch (error) {
         console.error("Ошибка загрузки:", error);
+        loading = false; // обработка ошибки, разрешаем загрузку следующей порции данных
     }
 }
 
 // Функция, которая отрисовывает список фильмов
 function updateContainer(data) {
     const container = document.querySelector(".content");
-    container.textContent = "";
 
-    data.items.forEach((film) => {
-        const item = createFilmItem(film); // Создает элемент фильма
-        container.appendChild(item);
-    });
-
-    managePageButtons(); //Управление кнопками предыдущая/следующая страницы
-    // attachFilmButtonsEvent(); // Переход на карточку выбранного пользователем фильма
-    attachLikeButtonsEvent(); // Передача id фильма в БД при нажатии на лайк
+    if (data && data.items && Array.isArray(data.items)) {
+        data.items.forEach((film) => {
+            const item = createFilmItem(film); // Создает элемент фильма
+            container.appendChild(item);
+        });
+    } else {
+        console.error('Неверный формат данных:', data);
+    }
 }
 
 // Создает элемент фильма. Проходимся по массиву фильмов и добавляем информацию в контейнер
 function createFilmItem(film) {
     const item = document.createElement("div");
     item.classList.add("content__item");
+
+    let titleLink = '';
+    let originalTitleLink = '';
+
+    // Проверка на null или undefined для film.nameRu
+    if (film.nameRu !== null && film.nameRu !== undefined) {
+        titleLink = `<a class="content__title" href="page-movie.html?id=${film.kinopoiskId}">${film.nameRu}</a>`;
+    }
+
+    // Проверка на null или undefined для film.nameOriginal
+    if (film.nameOriginal !== null && film.nameOriginal !== undefined) {
+        originalTitleLink = `<a class="content__original-title" href="page-movie.html?id=${film.kinopoiskId}">${film.nameOriginal}</a>`;
+    }
+
     const template =
         `
         <div class="content__poster">
-        <img src="${film.posterUrlPreview}" alt="poster" class="content__img">
-    </div>
-    <div class="content__info">
-        <a class="content__title" href="page-movie.html?id=${film.kinopoiskId}">${film.nameRu}</a>
-        <a class="content__original-title" href="page-movie.html?id=${film.kinopoiskId}">${film.nameOriginal}</a>
-        <div class="content__year">Год выхода фильма: ${film.year}</div>
-        <div class="content__rating">Рейтинг по кинопоиску: ${film.ratingKinopoisk}</div>
-        <div class="content__ratingImdb">Рейтинг по Imdb: ${film.ratingImdb}</div>
-        <div class="filmFavContainer">
-            <button class="content-button likeBtn" id="${film.kinopoiskId}">
-                <span class="likeIcon"></span>
-            </button>
+            <img src="${film.posterUrlPreview}" alt="poster" class="content__img">
         </div>
-   
+        <div class="content__info">
+            ${titleLink}
+            ${originalTitleLink}
+            <div class="content__year">Год выхода фильма: ${film.year}</div>
+            <div class="content__rating">Рейтинг по кинопоиску: ${film.ratingKinopoisk}</div>
+            <div class="content__ratingImdb">Рейтинг по Imdb: ${film.ratingImdb}</div>
+        </div>
         `;
+
     item.innerHTML = template;
     return item;
 }
-
-// // Обработчик события клика на кнопках для показа инфы о фильиах
-// function attachFilmButtonsEvent() {
-
-//     const buttonInfo = document.querySelectorAll('.content__btn')//находим все кнопки
-//     buttonInfo.forEach(function (button) { // проходим по каждой кнопке:
-
-//         button.addEventListener('click', function () {
-//             const filmId = button.getAttribute('id'); // Получаю id фильма из атрибута ID
-//             //console.log(filmId); //Проверка
-//             window.location.href = `page-movie.html?id=${filmId}`; // Переход на страницу film.html
-//         });
-//     });
-// }
 
 // Обработчик события клика на кнопках лайков
 function attachLikeButtonsEvent() {
@@ -109,7 +121,7 @@ function attachLikeButtonsEvent() {
                 putLike(target);
                 setLike(accessObj, filmId, true);
                 console.log(filmId);
-                
+
             }
         });
     });
@@ -121,7 +133,7 @@ function putLike(span) {
 }
 
 // Функция для работы с БД
- async function setLike(user_id, film_id, state) {
+async function setLike(user_id, film_id, state) {
     let subfield = `likes.${film_id}`;
     if (state) {
         const data = {};
@@ -131,118 +143,3 @@ function putLike(span) {
         await db.removeSubfield("users", user_id, subfield);
     }
 }
-
-// Управление кнопками страниц
-function managePageButtons() {
-    const paginationContainer = document.querySelector(".pagination");
-
-    if (!document.querySelector(".pagination__prev-page-btn") || !document.querySelector(".pagination__next-page-btn btn")) { // Добавить элементы управления страницами, если их нет
-        const templateBtn =
-            `
-        <button class="pagination__prev-page-btn btn">Предыдущая страница</button>
-        <button class="pagination__next-page-btn btn">Следующая страница</button>
-        `
-        paginationContainer.innerHTML = templateBtn;
-
-        prevPageBtn = document.querySelector(".pagination__prev-page-btn");
-        nextPageBtn = document.querySelector(".pagination__next-page-btn");
-
-        nextPageBtn.addEventListener("click", function (event) { //Обработчик события на кнопку "Следующая страница"
-            event.preventDefault();
-            getData(currentPage + 1);
-        });
-
-        prevPageBtn.addEventListener("click", function (event) { //Обработчик события на кнопку "Предыдущая страница"
-            event.preventDefault();
-            if (currentPage > 1) {
-                getData(currentPage - 1);
-            }
-        });
-    }
-    // Убрать кнопку "предыдущая" страница на первой и "Следующая" на послндней
-    prevPageBtn.style.display = (currentPage === 1) ? "none" : "block";
-    nextPageBtn.style.display = (currentPage === 4) ? "none" : "block"; //Апи выдает 5 страниц, но кнопка пропадает на 5-й только если здесь 4
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import { get } from './db';
-
-document.addEventListener("DOMContentLoaded", function () {
-    getMovieId();
-
-    // Получаем id фильма из бд у пользователя:
-    async function getMovieId() {
-        try {
-            // Получаем id пользователя из бд:
-            // const clientId = window.localStorage.getItem('client');
-            // const clientInfo = JSON.parse(clientId);
-            // const userId = clientInfo.id;
-
-            // const userData = await get("users", userId);
-
-            // console.log(userData);
-const ex={
-    "name": "Михаил",
-    "email": "bulka12121@gmail.com",
-    "secret": "8586446755a81d800522bacda9fcf9c2",
-    "likes": {
-        "326": true,
-        "361": true,
-        "389": true,
-        "435": true,
-        "351771": true,
-        "460089": true,
-        "568016": true,
-        "909720": true,
-        "1201206": true,
-        "1252447": true,
-        "1309325": true,
-        "5260016": true
-    },
-    "password": "1ac217343c079c21859c159049731109",
-    "id": "WAdKgR1PYL9r3fzKk03d"
-}
-            const likesKeys = Object.keys(ex.likes);
-            console.log(likesKeys);
-            await getMovies(likesKeys);
-        } catch (error) {
-            console.error('Ошибка при получении данных:', error);
-        }
-    }
-
-    async function getMovies(likesKeys) {
-        console.log("я срабатываю");
-        try {
-            const moviePromises = likesKeys.map(async (id) => {
-                const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}`, {
-                    method: "GET",
-                    headers: {
-                        'content-type': "application/json",
-                        "X-API-KEY": 'b5f46c64-de46-487e-b427-0ecc0ce121a5'
-                    },
-                });
-                const data = await response.json();
-                console.log(data);
-                // updateContainer(data);
-                return data; // возвращаем результат запроса
-            });
-
-            const moviesData = await Promise.all(moviePromises);
-            console.log(moviesData); // массив данных по фильмам
-        } catch (error) {
-            console.error("Ошибка загрузки:", error);
-        }
-    }
-});
