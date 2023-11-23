579b62a9c5141fcb12c39d78e5dd3c6f08093423
User Azizova <nikaazizova64@gmail.com>
2023-11-23 16:11:41 +0400

    renew

--- src/vars.js
+++ src/vars.js
@@ -6,12 +6,3 @@ export const upperPosters = document.getElementById('upper-posters');
 export const downPosters = document.getElementById('down-posters');
 export const div = document.createElement('div');
 export const divTwo = document.createElement('div');
-
-
-
-//Получаем элементы из html, главная страница
-
-export const upperPosters = document.getElementById('upper-posters');
-export const downPosters = document.getElementById('down-posters');
-export const div = document.createElement('div');
-export const divTwo = document.createElement('div');
